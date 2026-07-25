5cc2cdd2ba4633ba2de9b7210ac37009d31cfa83
manoj-sampang <iloveump40@gmail.com>
2026-07-26 00:45:55 +0545

    data-loaded successfully ndarray

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 # Project on Image Classfication (CIFAR-10)
 
+# loading the data_set from tensorflow keras
+
+from tensorflow.keras.datasets import cifar10
+
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+print(type(x_train))
+print(type(y_train))
+print(type(x_test))
+print(type(y_test))
+
+
+
+
 
