275f07419d2f618b868210c40a6e367cf940fca1
manoj-sampang <iloveump40@gmail.com>
2026-08-22 21:57:39 +0545

    model created

--- building_model.py
+++ building_model.py
@@ -2,6 +2,14 @@
 
 import tensorflow as tf
 
+# loading the keras dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+#normalize pixel values of the dataset
+x_train = x_train/255
+x_test = x_test/255
+
+#buidling CNN
 model = tf.keras.Sequential()
 
 model.add(tf.keras.Input(shape=(32, 32, 3)))
@@ -47,4 +55,21 @@ model.add(
     )
 )
 
+#model architecture
 model.summary()
+
+#configure learning 
+model.compile(
+        optimizer="adam",
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"]
+)
+
+#Training
+
+history = model.fit(
+        x_train,
+        y_train,
+        epochs=10,
+        validation_data=(x_test, y_test)
+)
